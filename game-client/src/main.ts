2c73ce443f93fd75acd24789aa47f5277b8d628a
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2023-11-08 14:46:06 +0900

    feat: client: Add Babylon.js

--- game-client/src/main.ts
+++ game-client/src/main.ts
@@ -1,24 +1,34 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import * as BABYLON from "@babylonjs/core";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+async function main() {
+  const canvas = document.getElementById("app") as HTMLCanvasElement;
+  const engine = new BABYLON.Engine(canvas, true);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const scene = new BABYLON.Scene(engine);
+
+  new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0, -1, 0.5), scene);
+  new BABYLON.ArcRotateCamera(
+    "camera",
+    -Math.PI / 2,
+    Math.PI / 2.5,
+    5,
+    BABYLON.Vector3.Zero(),
+    scene,
+  );
+
+  const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
+  box.rotation.x = Math.PI / 4;
+  box.rotation.z = Math.PI / 4;
+
+  engine.runRenderLoop(() => {
+    box.rotation.y += 0.001 * engine.getDeltaTime();
+    scene.render();
+  });
+
+  window.addEventListener("resize", () => {
+    engine.resize();
+  });
+}
+
+main();
